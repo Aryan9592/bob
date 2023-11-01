e0b73b0b234d18c904d0b4e65668bf3d03c12d83
Gregory Hill <gregorydhill@outlook.com>
2023-11-01 08:44:18 +0000

    refactor: use test light relay stub
    Signed-off-by: Gregory Hill <gregorydhill@outlook.com>

--- sdk/scripts/init-bridge.ts
+++ sdk/scripts/init-bridge.ts
@@ -24,7 +24,6 @@ main().catch((err) => {
 
 async function main(): Promise<void> {
     const electrs = new DefaultElectrsClient("testnet");
-    // args["parachain-endpoint"]
 
     const initHeight = args["init-height"];
     const privateKey = args["private-key"];
@@ -40,15 +39,14 @@ async function main(): Promise<void> {
     console.log(`Genesis: ${genesis}`);
     console.log(`beforeRetarget: ${beforeRetarget}`);
     console.log(`afterRetarget: ${afterRetarget}`);
-    
 
-    exec(`GENESIS_HEIGHT=${initHeight} GENESIS_HEADER=${genesis} RETARGET_HEADERS=${beforeRetarget}${afterRetarget} PRIVATE_KEY=${privateKey} forge script script/Bridge.s.sol:BridgeScript --rpc-url 'https://l2-fluffy-bob-7mjgi9pmtg.t.conduit.xyz' --chain 901 --verify --verifier blockscout --verifier-url 'https://explorerl2-fluffy-bob-7mjgi9pmtg.t.conduit.xyz/api?' --broadcast`, 
+    exec(`GENESIS_HEIGHT=${initHeight} GENESIS_HEADER=${genesis} RETARGET_HEADERS=${beforeRetarget}${afterRetarget} PRIVATE_KEY=${privateKey} forge script script/Bridge.s.sol:BridgeScript --rpc-url 'https://l2-fluffy-bob-7mjgi9pmtg.t.conduit.xyz' --chain 901 --verify --verifier blockscout --verifier-url 'https://explorerl2-fluffy-bob-7mjgi9pmtg.t.conduit.xyz/api?' --broadcast`,
         (err: any, stdout: any, stderr: any) => {
-        if (err) {
-            throw new Error(`Failed to run command: ${err}`);
-        }
-      
-        // the *entire* stdout and stderr (buffered)
-        console.log(`stdout: ${stdout}`);
-      });
+            if (err) {
+                throw new Error(`Failed to run command: ${err}`);
+            }
+
+            // the *entire* stdout and stderr (buffered)
+            console.log(`stdout: ${stdout}`);
+        });
 }
